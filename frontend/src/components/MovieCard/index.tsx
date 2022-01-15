083c8643c452acbe863dd813c66f44fccd4ddda0
vitor18alves <vitor18alves@gmail.com>
2022-01-15 20:06:38 -0300

    Props

--- frontend/src/components/MovieCard/index.tsx
+++ frontend/src/components/MovieCard/index.tsx
@@ -1,17 +1,14 @@
 import MovieScore from "components/MovieScore";
 import { Link } from "react-router-dom";
+import { Movie } from "types/movie";
 
 
+type Props = {
+    movie: Movie;
+}
 
-function MoveCard() {
 
-    const movie = {
-        id: 1,
-        image: "https://www.themoviedb.org/t/p/w533_and_h300_bestv2/jBJWaqoSCiARWtfV0GlqHrcdidd.jpg",
-        title: "The Witcher",
-        count: 2,
-        score: 4.5
-    };
+function MoveCard( { movie } : Props) {
 
     return(
 <div>
