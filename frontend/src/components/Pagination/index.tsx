2f57042fb30d402d20c3830443be28e879fbae64
vitor18alves <vitor18alves@gmail.com>
2022-01-16 15:10:55 -0300

    Show score

--- frontend/src/components/Pagination/index.tsx
+++ frontend/src/components/Pagination/index.tsx
@@ -15,7 +15,6 @@ function Pagination() {
                 </button>
             </div>
         </div>
-
     );
 }
 
--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -22,7 +22,7 @@ function Listing() {
     });
 
     useEffect(() => {
-        axios.get(`${BASE_URL}/movies?size=12&page=${pageNumber}&sort=title`)
+        axios.get(`${BASE_URL}/movies?size=12&page=${pageNumber}`)
             .then(response => {
                 const data = response.data as MoviePage;
                 setPage(data);
@@ -37,12 +37,11 @@ function Listing() {
 
             <div className="container">
                 <div className="row">
-
                     {page.content.map(movie => (
                         <div  key={movie.id} className="col-5m-6 col-lg-4 col-xl-3 mb-3">
                             <MovieCard movie={movie} />
                         </div>
-                    )
+                        )
                     )}
                 </div>
             </div>
